fb9e3e948ea36c200b3abf0a21b64430f43919f6
Alex Rakovskiy <a.rakovskiy@gmail.com>
2023-01-25 11:48:12 +0000

    Annotations added

--- main.py
+++ main.py
@@ -1,29 +1,19 @@
-"""
-Character creation module
-"""
-
 from random import randint
 
 
-def attack(char_name, char_class):
-    """
-    Sets the character damage
-    """
+def attack(char_name: str, char_class: str) -> str:
     if char_class == 'warrior':
-        return f'''{char_name} нанёс урон противнику равный
-                   {5 + randint(3, 5)}'''
+        return (f'{char_name} нанёс урон противнику равный'
+                f'{5 + randint(3, 5)}')
     if char_class == 'mage':
-        return f'''{char_name} нанёс урон противнику равный
-                   {5 + randint(5, 10)}'''
+        return (f'{char_name} нанёс урон противнику равный'
+                f'{5 + randint(5, 10)}')
     if char_class == 'healer':
-        return f'''{char_name} нанёс урон противнику равный
-                   {5 + randint(-3, -1)}'''
+        return (f'{char_name} нанёс урон противнику равный'
+                f'{5 + randint(-3, -1)}')
 
 
-def defence(char_name, char_class):
-    """
-    Sets the character block
-    """
+def defence(char_name: str, char_class: str) -> str:
     if char_class == 'warrior':
         return f'{char_name} блокировал {10 + randint(5, 10)} урона'
     if char_class == 'mage':
@@ -32,25 +22,19 @@ def defence(char_name, char_class):
         return f'{char_name} блокировал {10 + randint(2, 5)} урона'
 
 
-def special(char_name, char_class):
-    """
-    Sets the character special ability
-    """
+def special(char_name: str, char_class: str) -> str:
     if char_class == 'warrior':
-        return f'''{char_name} применил специальное умение
-                   «Выносливость {80 + 25}»'''
+        return (f'{char_name} применил специальное умение'
+                f'«Выносливость {80 + 25}»')
     if char_class == 'mage':
-        return f'''{char_name} применил специальное умение
-                   «Атака {5 + 40}»'''
+        return (f'{char_name} применил специальное умение'
+                f'«Атака {5 + 40}»')
     if char_class == 'healer':
-        return f'''{char_name} применил специальное умение
-                   «Защита {10 + 30}»'''
+        return (f'{char_name} применил специальное умение'
+                f'«Защита {10 + 30}»')
 
 
-def start_training(char_name, char_class):
-    """
-    Sets the tutorial
-    """
+def start_training(char_name: str, char_class: str) -> str:
     if char_class == 'warrior':
         print(f'{char_name}, ты Воитель — отличный боец ближнего боя.')
     if char_class == 'mage':
@@ -63,7 +47,7 @@ def start_training(char_name, char_class):
           'defence — чтобы блокировать атаку противника, '
           'special — чтобы использовать свою суперсилу.')
     print('Если не хочешь тренироваться, введи команду skip.')
-    cmd = None
+    cmd: str = None
     while cmd != 'skip':
         cmd = input('Введи команду: ')
         if cmd == 'attack':
@@ -75,12 +59,9 @@ def start_training(char_name, char_class):
     return 'Тренировка окончена.'
 
 
-def choice_char_class():
-    """
-    Sets the class choice
-    """
-    approve_choice = None
-    char_class = None
+def choice_char_class() -> str:
+    approve_choice: str = ''
+    char_class: str = ''
     while approve_choice != 'y':
         char_class = input('Введи название класса, за который хочешь играть: '
                            'Воитель — warrior, '
@@ -102,18 +83,14 @@ def choice_char_class():
 
 
 def main():
-    """
-    Sets the intro
-    """
     print('Приветствую тебя, искатель приключений!')
     print('Прежде чем начать игру...')
-    char_name = input('...назови себя: ')
+    char_name: str = input('...назови себя: ')
     print(f'Здравствуй, {char_name}! '
           'Сейчас твоя выносливость — 80, атака — 5 и защита — 10.')
     print('Ты можешь выбрать один из трёх путей силы:')
     print('Воитель, Маг, Лекарь')
-    char_class = choice_char_class()
+    char_class: str = choice_char_class()
     print(start_training(char_name, char_class))
 
-
-main()
+    main()
